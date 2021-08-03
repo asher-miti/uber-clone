35a4b5a07c22454d8a8ff84d52a7ed18da4ccf57
Asher Mitilinakis <asher_3@hotmail.co.uk>
2021-08-03 23:20:47 +0100

    adding map and react navigation

--- App.js
+++ App.js
@@ -4,11 +4,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -8,6 +8,8 @@ import {
   View,
 } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
   {
@@ -25,18 +27,27 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
           <Image
             source={{ uri: item.image }}
             style={{ width: 120, height: 120, resizeMode: 'contain' }}
           />
           <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+          <Icon
+            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            type='antdesign'
+            name='arrowright'
+            color='white'
+          />
         </TouchableOpacity>
       )}
     />
