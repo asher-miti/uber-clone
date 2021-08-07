bb78db134e96be2b021aa8c6ee64abbd01447473
Asher Mitilinakis <asher_3@hotmail.co.uk>
2021-08-07 13:00:18 +0100

    Adding navigate and rideoptions cards

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -10,6 +10,8 @@ import {
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [
   {
@@ -28,6 +30,7 @@ const data = [
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
   return (
     <FlatList
       data={data}
@@ -36,18 +39,21 @@ const NavOptions = () => {
       renderItem={({ item }) => (
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
-          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: 120, height: 120, resizeMode: 'contain' }}
-          />
-          <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-          <Icon
-            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-            type='antdesign'
-            name='arrowright'
-            color='white'
-          />
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}>
+          <View style={tw`${!origin && 'opacity-20'}`}>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type='antdesign'
+              name='arrowright'
+              color='white'
+            />
+          </View>
         </TouchableOpacity>
       )}
     />
